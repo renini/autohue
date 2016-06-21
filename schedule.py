e564695bda3f257570bb7ede62b62fd23fa0b03d
wcktklwn <wcktklwn@wcktklwn.net>
2016-06-20 19:28:11 -0600

    Fix a couple of things.

--- schedule.py
+++ schedule.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 # Set city
-city_name = 'Denver'
+city_name = 'city'
 
 
 # Import all needed modules.
@@ -21,7 +21,7 @@ b = Bridge('ip_of_your_bridge')
 b.connect()
 b.get_api()
 logging.basicConfig()
-
+lights = 'off'
 
 
 # Astral Setup
@@ -29,18 +29,22 @@ logging.basicConfig()
 a = Astral()
 a.solar_depression = 'civil'
 city = a[city_name]
-sun = city.sun(date=datetime.datetime.today(), local=True)
 
 
 
 # Check Sunset and Sunrise and turn lights on
 
 while True:
-        if datetime.datetime.now(pytz.timezone('US/Mountain')) > sun['sunset'] a                                                                nd datetime.datetime.now(pytz.utc) > sun['sunrise']:
-                b.set_light([4,5,6], 'on', True)
-                b.set_light([4,5,6], 'bri', 254)
-        elif datetime.datetime.now(pytz.timezone('US/Mountain')) > sun['sunrise'                                                                ] and datetime.datetime.now(pytz.utc) < sun['sunset']:
-                lights_list = b.get_light_objects('list')
-                for light in lights_list:
-                  light.on = True
+        sun = city.sun(date=datetime.datetime.today(), local=True)
+        if datetime.datetime.now(pytz.timezone('US/Mountain')) > sun['sunset'] and datetime.datetime.now(pytz.utc) > sun['sunrise']:
+                if lights == 'off':
+                        b.set_light([4,5,6], 'on', True)
+                        b.set_light([4,5,6], 'bri', 254)
+                        lights = 'on'
+        elif datetime.datetime.now(pytz.timezone('US/Mountain')) > sun['sunrise'] and datetime.datetime.now(pytz.utc) < sun['sunset']:
+                if lights == 'on':
+                        lights_list = b.get_light_objects('list')
+                        for light in lights_list:
+                                light.on = False
+                        lights = 'off'
         time.sleep(60)
